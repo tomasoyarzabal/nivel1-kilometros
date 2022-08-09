321b34e1f65dea8b066f73456368e9742ab1c9e8
tomasoyarzabal <tomasoyarzabal135@gmail.com>
2022-08-09 10:05:16 -0300

    kilometros

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace kilometros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float distancia,velocidad,tiempo;
+            Console.WriteLine("ingrese distancia");
+            distancia = float.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese velocidad");
+            velocidad = float.Parse(Console.ReadLine());
+            tiempo = distancia/velocidad;
+            Console.WriteLine("el tiempo que tardara es de " + tiempo.ToString("0.00") + " horas");
+
+        
+
+        }
+    }
+}
